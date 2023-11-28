7dc4e80d93869f2b1f974aad1659adbf4ad114cc
Xuan <myatthiha.soe.9@gmail.com>
2023-11-28 11:45:33 +0630

    creating conditon

---
+++ condition.php
@@ -0,0 +1,7 @@
+<?php
+    $BlackjackPlayer1 = 20;
+    $BlackjackPlayer1 <= 21;
+    $Result = "Player 1 is still in the game.";
+    $Result = "Player 1 is out of the action.";
+    echo "<p>", $Result,"</p>";
+?>
